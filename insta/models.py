738083f281cd5cf305725de53d5ff63e55c2b000
kaphie <bkafrika144@gmail.com>
2020-05-30 22:02:00 +0300

    adding content to admin.py

--- insta/models.py
+++ insta/models.py
@@ -7,3 +7,8 @@ class Post(models.Model):
     image = models.ImageField(blank=True,null=True)
     caption = models.TextField()
     created_date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.caption
+
+     
